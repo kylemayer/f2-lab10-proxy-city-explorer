bc01e439bac35e5d2e6df5e4a4ec2a60d3eecd44
Kyle Mayer <x@kylemayer.dev>
2021-06-24 13:10:13 -0700

    setup files

---
+++ __tests__/munge.test.js
@@ -0,0 +1,19 @@
+
+
+require('dotenv').config();
+
+describe('munge', () => {
+
+    test('location data', async() => {
+
+    const expectation = {
+
+    };
+
+    const input = '';
+
+    const actual = mungeLocationResponse(input);
+
+    expect(actual).toEqual(expectation);
+    });
+});
