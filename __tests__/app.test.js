80c8a03005acecff3031be328f7733827cfa471c
Kyle Mayer <x@kylemayer.dev>
2021-06-24 17:28:30 -0700

    location and weather tests pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,67 @@
 require('dotenv').config();
+const { mungeLocationRes, mungeWeatherRes, mungeReviewRes } = require('../lib/mungeUtils.js');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
+const locationData = require('../data/locationData.json');
+const weatherData = require('../data/weatherData.json');
+const reviewsData = require('../data/reviewsData.json');
 
 describe('app routes', () => {
+  describe('routes', () => {
 
-  test('location', async() => {
+    test('returns munged location data', () => {
 
-    const expectation = [
+      const location = locationData;
 
-    ];
+      const expectation =   {
+        formatted_query: 'Portland, Multnomah County, Oregon, USA',
+        latitude: '45.5202471',
+        longitude: '-122.6741949'
+      };
 
-    const data = await fakeRequest(app)
-      .get('/location....')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = mungeLocationRes(location);
 
-    expect(data.body).toEqual(expectation);
+      expect(data).toEqual(expectation);
+    });
+
+    test('returns munged weather data', () => {
+
+      const weather = weatherData;
+
+      const expectation = [
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) },
+        { forecast: expect.any(String), time: expect.any(String) }
+      ];
+
+      const data = mungeWeatherRes(weather);
+
+      expect(data).toEqual(expectation);
+    });
+
+    test('returns munged yelp reviews data', () => {
+
+      const reviews = reviewsData;
+
+      const expectation =   [];
+
+      const data = mungeReviewRes(reviews);
+
+      expect(data).toEqual(expectation);
+    });
+
   });
 });
+
